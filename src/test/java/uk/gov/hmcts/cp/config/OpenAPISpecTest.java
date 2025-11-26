02790e4523eea4881c5270785e76d135c4cfcc54
coling01 <colin.greenwood@hmcts.net>
2025-11-26 18:39:46 +0000

    chore: drop open api loader and add simple test

---
+++ src/test/java/uk/gov/hmcts/cp/config/OpenAPISpecTest.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.cp.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.cp.openapi.api.CaseIdByCaseUrnApi;
+import uk.gov.hmcts.cp.openapi.model.CaseMapperResponse;
+import uk.gov.hmcts.cp.openapi.model.ErrorResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class OpenAPISpecTest {
+    @Test
+    void generated_error_response_should_have_expected_fields() {
+        assertThat(ErrorResponse.class).hasDeclaredFields("error", "message", "details", "traceId");
+    }
+
+    @Test
+    void generated_case_mapper_response_should_have_expected_fields() {
+        assertThat(CaseMapperResponse.class).hasDeclaredFields("caseId", "caseUrn");
+    }
+
+    @Test
+    void generated_api_should_have_expected_methods() {
+        assertThat(CaseIdByCaseUrnApi.class).hasDeclaredMethods("getCaseIdByCaseUrn", "getRequest");
+    }
+}
